89a7852a6cf6d737b3aee081d4731039f40c3784
alchonokk <galunia_suglob@mail.ru>
2021-08-21 01:26:59 +0300

    feat: add function for negative int

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 module.exports = function reverse (n) {
- 
+ if (n<0){
+     n=n*(-1);
+     let reversed=0
+    while (n) {
+        reversed= reversed*10+n%10;
+        n=Math.floor(n/10)
+    }
+    reversed=reversed
+    return reversed
+ }
+
+
     let reversed=0
     while (n) {
         reversed= reversed*10+n%10;
         n=Math.floor(n/10)
         
     }
-return reversed
+    return reversed
 }
